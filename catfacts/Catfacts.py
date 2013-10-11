d6db3b30a7f99d84b781bd0f2ab27860fbbca5ef
laingsoft <cclaing@ualberta.ca>
2013-10-11 08:46:31 -0600

    Main commit

---
+++ catfacts/Catfacts.py
@@ -0,0 +1,49 @@
+import smtplib, random
+def catfacts():
+    work=open('catfacts.txt','r')
+    lines = work.readlines()
+    message = lines[random.randint(0,((len(lines)-1)))]
+    vtext = "7809073336"
+    provider = "telus"
+    str.lower(provider)
+    if provider == "telus":
+        vtext = vtext+'@msg.telus.com'
+    elif provider == "rogers":
+        vtext = vtext+'@pcs.rogers.com'
+    elif provider == "bell":
+        vtext = vtext+'@txt.bell.ca'
+    elif provider == "fido":
+        vtext = vtext+'@sms.fido.ca'
+    elif provider == "koodo":
+        vtext = vtext+'@msg.telus.com'
+    elif provider == "wind":
+        vtext = vtext+'@txt.windmobile.ca'
+
+    mailsend(vtext,message)
+
+    
+
+def mailsend(vtext,message):
+
+    username = #email goes here
+    password = #password goes here
+
+    #vtext = Email to send it to.
+    #message = The message to send.
+
+    msg = """From: {0}
+    To: You
+
+
+    
+    {1}
+    """ .format (username,message)
+
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(username, vtext, msg)
+    server.quit()
+    print 'Done!'
+while True:   
+    catfacts()
